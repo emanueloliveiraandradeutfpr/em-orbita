de02c6f3b5bb1eb868fc27f4e4b2d478c2d1b7aa
emanueloliveiraandrade <emanueloliveiraandrade@alunos.utfpr.edu.br>
2024-09-22 20:36:34 -0300

    chore: assets directory add to the build

--- server.ts
+++ server.ts
@@ -45,6 +45,7 @@ export function app(): express.Express {
 }
 
 function run(): void {
+  // biome-ignore lint/complexity/useLiteralKeys: <explanation>
   const port = process.env['PORT'] || 4000;
 
   // Start up the Node server
