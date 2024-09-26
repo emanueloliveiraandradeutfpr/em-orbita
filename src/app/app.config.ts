57ba86724661323e6939095ad0cb67d7b20bfbf8
emanueloliveiraandrade <emanueloliveiraandrade@alunos.utfpr.edu.br>
2024-09-25 23:58:31 -0300

    chore: config angular material

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,11 +1,16 @@
-import { provideZoneChangeDetection } from '@angular/core';
-import type { ApplicationConfig} from '@angular/core';
-import { provideRouter } from '@angular/router';
+import { provideZoneChangeDetection } from '@angular/core'
+import type { ApplicationConfig } from '@angular/core'
+import { provideRouter } from '@angular/router'
 
-import { routes } from './app.routes';
-import { provideClientHydration } from '@angular/platform-browser';
-import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
+import { routes } from './app.routes'
+import { provideClientHydration } from '@angular/platform-browser'
+import { provideAnimationsAsync } from '@angular/platform-browser/animations/async'
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes), provideClientHydration(), provideAnimationsAsync()]
-};
+  providers: [
+    provideZoneChangeDetection({ eventCoalescing: true }),
+    provideRouter(routes),
+    provideClientHydration(),
+    provideAnimationsAsync(),
+  ],
+}
